5acbc6c1704714d02545caba3d58dbb2f33e6014
Ruben Santos <ruben.d.santos@tecnico.ulisboa.pt>
2017-04-18 03:50:13 +0100

    improved style

--- mymusic/myrepository/forms.py
+++ mymusic/myrepository/forms.py
@@ -9,7 +9,7 @@ BIRTH_YEAR_CHOICES =  range(timezone.now().year - 100, timezone.now().year)
 class AlbumForm(forms.ModelForm):
 	class Meta:
 		model = Album
-		fields = ['title','a_date','favorite','n_songs','description','genres','types'] 
+		fields = ['id','title','a_date','favorite','n_songs','description','genres','types'] 
 
 	title = forms.CharField(max_length=100)
 	a_date = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
